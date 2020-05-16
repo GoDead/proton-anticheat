fd37f21526cff4cb8ac5edcd980aacd4823a68a4
GoDead <51884609+GoDead@users.noreply.github.com>
2020-05-16 12:07:07 +0200

    Checks if the player is falling or on water

--- src/main/java/eu/drham/listener/DetectionListener.java
+++ src/main/java/eu/drham/listener/DetectionListener.java
@@ -11,6 +11,9 @@ import org.bukkit.event.player.PlayerMoveEvent;
 
 public class DetectionListener implements Listener {
 
+    static boolean isFalling;
+	static boolean isOnWater;
+    
     @EventHandler
     public void onHit(EntityDamageByEntityEvent e){
       //  if(e.getDamager() instanceof Player) {
@@ -23,6 +26,22 @@ public class DetectionListener implements Listener {
     public void onMove(PlayerMoveEvent e){
         Profile.getProfile(e.getPlayer().getUniqueId()).getCheck(CheckType.SPEED).runCheck(e);
     }
+    
+    @EventHandler
+	public void fallEvent(PlayerMoveEvent event) {
+		isFalling = event.getTo().getY() + 0.5 < event.getFrom().getY();
+        /*returns true if the player is falling*/
+		//isGoingUp = event.getTo().getY() > event.getFrom().getY() + 0.2;
+		//int underY = event.getPlayer().getLocation().getBlockY() - 1;
+		Location location = event.getPlayer().getLocation().clone().add(0, -1, 0);
+
+		if (location.getBlock().getType() == Material.WATER) {
+			isOnWater = true;
+            /*returns true if the player is on water for liquidwalk*/
+		} else {
+			isOnWater = false;
+		}
+	}
 
 
 }
